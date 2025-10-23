3cd4cbb92dca144df05e1ffae1346ef82e9b9c77
Alejandro Flores <aflomen115@g.educaand.es>
2025-10-23 13:51:35 +0200

    Programa interactive

--- hola.php
+++ hola.php
@@ -1,5 +1,8 @@
-// El nombre por defecto es Mundo
 <?php
+// Autor: Manuel Paniagua <mpaniagua@iestrassierra.com>
+// El nombre por defecto es Mundo
+require('HolaMundo.php');
+print "Introduce tu nombre";
+$nombre = trim(fgets(STDI));
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
-#print "Hola, {$nombre}\n";
-?>
+print new HolaMundo($nombre);
